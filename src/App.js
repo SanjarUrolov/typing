1c94754c3bf3b9babd66f55feb1664da28b9a9d7
Sanjarbek Urolov <107649644+sanjarbekurolov1121@users.noreply.github.com>
2022-08-25 16:34:29 -0700

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import React, { useState } from "react";
 import "./style.css";
 
-let text = `Reakt interfaol foydalanuvchi interfeyslarni qiyinchiliksiz yaratish imkonini beradi. ilovangizdagi har bir holat uchun oddiy ko'rinishni loyihalashtiring va  react sizning ma'lumotlaringizni faqat o'zgartirgan tarkibiy qismlarni belgilab ularni samarali ravishda yangilaydi.
-deklarativ ko'rinishlar sizning kodingizni oldindan bashorat qilish va xatoliklarni onsonroq aniqlashga yordam beradi.`;
+let text = `React interfaol foydalanuvchi interfeyslarni qiyinchiliksiz yaratish imkonini beradi. Ilovangizdagi har bir holat uchun oddiy ko'rinishni loyihalashtiring va  react sizning ma'lumotlaringizni faqat o'zgartirgan tarkibiy qismlarni belgilab ularni samarali ravishda yangilaydi.
+Deklarativ ko'rinishlar sizning kodingizni oldindan bashorat qilish va xatoliklarni onsonroq aniqlashga yordam beradi.`;
 const myArr = text.toLowerCase().split("");
 
 function App() {
-  const [inputText , setInputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [date, setDate]=useState(60)
+  console.log(date)
   const handleChange = (e) => {
     setInputText(e.target.value.toLowerCase());
   };
+  
+  // let date = 60;
+ let a=setInterval(()=>{
+      if (date > 0 && inputText) 
+      {date--
+        document.getElementById("date").innerHTML =
+           date + " soniya";
+    }
+    // if(date<=0){
+    //   date=60
+    // }
+  },1000)
+  clearInterval(a)
+  let count=0;
   return (
     <div className="app">
-    <h1>App</h1>
+      <h1><span class="date" id="date">
+         60 soniya
+      </span>
+      </h1>
+      
+      {/* <button onClick={a()}>Click</button> */}
       <div className="text">
         {myArr.map((value, index) => {
-         {/* if(inputText > 0){
+          {
+            /* if(inputText > 0){
           <span style={{ color: "black" , fontWeight:700 }} key={index}>
                 {value}
               </span>
-            } */}
+            } */
+          }
 
           if (inputText[index] === value) {
+                 count++;
             return (
-              <span style={{ color: "green" , fontWeight :800 ,  }} key={index}>
+              <span style={{ color: "green", fontWeight: 800 }} key={index}>
                 {value}
               </span>
             );
-          } 
-
-          else if(inputText[index] !== value){
+          } else if (inputText[index] !== value) {
             return (
-              <span style={{ color: "black" , opacity: "0.5" }} key={index}>
+              <span style={{ color: "black", opacity: "0.5" }} key={index}>
                 {value}
               </span>
             );
@@ -47,6 +69,7 @@ function App() {
           placeholder="Start typing. . . "
         />
       </form>
+      <h2>{count}</h2>
     </div>
   );
 }
